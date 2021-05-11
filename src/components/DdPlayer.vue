ab51dfbdb6e6d7d3869c2dcaa1e673440ab0529e
Richard <wcsz1987@foxmail.com>
2021-05-11 23:56:53 +0800

    修复控制台报错

--- src/components/DdPlayer.vue
+++ src/components/DdPlayer.vue
@@ -58,10 +58,7 @@ export default {
         language: 'zh-CN',
         // aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
         fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        sources: [{
-          type: "video/mp4", // 类型
-          src: '' // url地址
-        }],
+        sources: [],
         poster: '', // 封面地址
         notSupportedMessage: '此视频暂无法播放，请稍后再试', // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
         controlBar: {
